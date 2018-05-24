27c530b91e95ecb8fd270c04f2123e90c8e92c0a
1912726 <1912726@00-46242.pusd.dom>
2018-05-24 15:09:31 -0700

    player1 and player2 classes

--- Final/src/Player2.java
+++ Final/src/Player2.java
@@ -1,4 +1,10 @@
 
-public class Player2 {
-
+public class Player2 extends player
+{
+	private int direction;
+	private Location location;
+	public Player2(int direction, Location location)
+	{
+		super(direction, location);
+	}
 }

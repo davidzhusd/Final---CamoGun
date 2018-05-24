6b0c79ced391b3bae1ad83b64075fc1d9b85f247
1902345 <1902345@00-46284.pusd.dom>
2018-05-24 14:50:03 -0700

    new Grid

--- Final/src/Grid.java
+++ Final/src/Grid.java
@@ -3,12 +3,12 @@ public class Grid {
 
 	private int numCol;
 	private int numRow;
-	private Actor[][] grid;
+	private Tile[][] grid;
 	public Grid(int row, int col)
 	{
 		numCol = col;
 		numRow = row;
-		grid = new Actor[row][col];
+		grid = new Tile[row][col];
 	}
 	
 }
